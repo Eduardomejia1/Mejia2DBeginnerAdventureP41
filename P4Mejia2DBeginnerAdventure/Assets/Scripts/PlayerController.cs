5ea5f3cb39b03d81dcb0846ecf4b4d497795936e
Eduardomejia1 <730845@student.dusd.net>
2025-11-06 11:44:19 -0800

    Clone repository
    Clone

---
+++ P4Mejia2DBeginnerAdventure/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        { }
+}
